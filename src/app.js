54e11348d4f0b1797c6d5178ecdd4f25677ba053
DmitrCambur <camburdima@hotmail.com>
2025-06-03 19:09:40 +0200

    enabled CORS

--- src/app.js
+++ src/app.js
@@ -7,12 +7,14 @@ const currentIncomeRoutes = require("./routes/currentIncome");
 const historyExpenseRoutes = require("./routes/historyExpense");
 const historyIncomeRoutes = require("./routes/historyIncome");
 const indexRoutes = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 
 connectDB();
 
+app.use(cors());
 app.use("/", indexRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/months", monthRoutes);
